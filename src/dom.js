0a9d96043cce26344c8e92e28348614e424e22e3
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-08-15 21:29:48 +0000

    Feat: start Screen scaffolding

--- src/dom.js
+++ src/dom.js
@@ -15,24 +15,72 @@ export function boardLayout(size, coords, type, title = "") {
   boardName.textContent = title;
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  app.append( boardName, board);
+  app.append(boardName, board);
   return app;
 }
 
 export function gameOverScreen(winner) {
-  const gameOver = elements('div');
-  gameOver.classList.add('gameOver');
-  const declaration = elements('div')
-  const p = elements('p')
+  const gameOver = elements("div");
+  gameOver.classList.add("gameOver");
+  const declaration = elements("div");
+  const p = elements("p");
   p.textContent = `${winner.toUpperCase()} WINS`;
-  const StartButton = elements('button');
-  StartButton.textContent = 'Play Again'
-  StartButton.id = 'start';
-  StartButton.classList.add('start')
-  declaration.classList.add('declaration');
-  declaration.append(p,StartButton);
-  gameOver.append(declaration)
-  app.append(gameOver)
+  const StartButton = elements("button");
+  StartButton.textContent = "Play Again";
+  StartButton.id = "start";
+  StartButton.classList.add("start");
+  declaration.classList.add("declaration");
+  declaration.append(p, StartButton);
+  gameOver.append(declaration);
+  app.append(gameOver);
+}
+
+export function startScreen(init, createShips, handleAttack) {
+  const startContainer = elements("form");
+  const inputs = elements("input", 2);
+  const buttons = elements("button");
+  startContainer.classList.add("startScreen");
+  inputs[0].classList.add("playerName");
+  inputs[0].type = "text";
+  inputs[0].placeholder = "Name Commander";
+  inputs[1].classList.add("boardSize");
+  inputs[1].type = "number";
+  inputs[1].min = 8;
+  inputs[1].max = 10;
+  inputs[1].placeholder = 10;
+  buttons.classList.add("startButton");
+  buttons.textContent = "Start";
+  buttons.type = "submit";
+
+
+  startContainer.addEventListener("submit", (e) => {
+    e.preventDefault();
+    startContainer.style.display = "none";
+    if(inputs[1].value == ''){
+      inputs[1].value = '10';
+    }
+    const playerShipCoords = {
+      Carrier: ["A1", 1],
+      Battleship: ["C5", 1],
+      Cruiser: ["F1", 2],
+      Submarine: ["D6", 2],
+      Destroyer: ["J1", 2],
+    };
+    const computerShipCoords = {
+      Carrier: ["D1", 1],
+      Battleship: ["A6", 2],
+      Cruiser: ["A1", 2],
+      Submarine: ["B4", 2],
+      Destroyer: ["I3", 2],
+    };
+
+    const initPlayers = init(inputs[0].value, inputs[1].value);
+    createShips(initPlayers, playerShipCoords, computerShipCoords);
+    handleAttack(initPlayers);
+   
+  });
+  startContainer.append(inputs[0], inputs[1], buttons);
+  app.append(startContainer);
 }
 
 function elements(type, amount = 1) {
--- src/main.js
+++ src/main.js
@@ -1,35 +1,10 @@
 import { handleAttack } from "./handleAttacks";
 import { initializePlayers } from "./createPlayers";
 import { createShips } from "./createShips";
-
+import { startScreen } from "./dom";
 
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
-  //const playerName = prompt("What is your name");
-  //let size = prompt('Chose the size of you battle field')
-
-  const playerName = "him";
-  const size = 10;
-  const initPlayers = initializePlayers(playerName, size);
-  const playerShipCoords = {
-    Carrier: ["A1", 1],
-    Battleship: ["C5", 1],
-    Cruiser: ["F1", 2],
-    Submarine: ["D6", 2],
-    Destroyer: ["J1", 2],
-  };
-  const computerShipCoords = {
-    Carrier: ["D1", 1],
-    Battleship: ["A6", 2],
-    Cruiser: ["A1", 2],
-    Submarine: ["B4", 2],
-    Destroyer: ["I3", 2],
-  };
-
-  createShips(initPlayers, playerShipCoords, computerShipCoords);
-  handleAttack(initPlayers);
-  //player1's turn
-  //displayedBoard(true);
-  
+  startScreen(initializePlayers, createShips, handleAttack);
 }
